0550502720dacc53f041594a3772a515f9014e82
Alexander Refsum Jensenius <arjensenius@gmail.com>
2025-09-08 05:59:53 +0200

    add audio files

---
+++ scripts/rename_audio_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Rename audio files to be web-safe (lowercase, underscores, no spaces or special chars)
+
+mv "week5_Audio Ex- 12 - Jackie wilson .wav" week5_audio_ex_12_jackie_wilson.wav
+mv "week5_Audio Ex.  11 - bootsy riff 2 clave overlaid.wav" week5_audio_ex_11_bootsy_riff2_clave_overlaid.wav
+mv "week5_Audio Ex. 1 - Springar.mp3" week5_audio_ex_1_springar.mp3
+mv "week5_Audio Ex. 10 - bootsy riff 1 w drums.wav" week5_audio_ex_10_bootsy_riff1_w_drums.wav
+mv "week5_Audio Ex. 2 - Coltrane_MyFavThings.mp3" week5_audio_ex_2_coltrane_myfavthings.mp3
+mv "week5_Audio Ex. 3 - riff 2 REPEATED drums.wav" week5_audio_ex_3_riff2_repeated_drums.wav
+mv "week5_Audio Ex. 4 - dangelo_left&right.mp3" week5_audio_ex_4_dangelo_left_and_right.mp3
+mv "week5_Audio Ex. 5 - jBrown_getUpOffa.mp3" week5_audio_ex_5_jbrown_getupoffa.mp3
+mv "week5_Audio Ex. 6 - bootsy riff 2 REPEATED no drums.wav" week5_audio_ex_6_bootsy_riff2_repeated_no_drums.wav
+mv "week5_Audio Ex. 7 - James Brown - Get Up (Sex Machine) bin.mp3" week5_audio_ex_7_james_brown_getup_sex_machine_bin.mp3
+mv "week5_Audio Ex. 8 - James Brown - Get Up (Sex Machine) tri.mp3" week5_audio_ex_8_james_brown_getup_sex_machine_tri.mp3
+mv "week5_Audio Ex. 9 - James Brown - Get Up (Sex Machine) ori.mp3" week5_audio_ex_9_james_brown_getup_sex_machine_ori.mp3
+
+echo "Audio files renamed to web-safe filenames."
